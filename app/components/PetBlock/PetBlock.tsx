a640d26cdb909929ff7ff58c344349f8a3321dd1
Dmytro Farbun <dimofarb@gmail.com>
2026-06-07 21:14:47 +0300

    feat: update PetBlock with adding 1280px layout response, add altStyles for AddPet page,  fix background img positioning

--- app/components/PetBlock/PetBlock.tsx
+++ app/components/PetBlock/PetBlock.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import css from "./PetBlock.module.css";
 import Icon from "../Icon/Icon";
 
@@ -12,7 +11,6 @@ type PetBlockProps = {
   mob: ImageSrcs;
   tab: ImageSrcs;
   desk: ImageSrcs;
-  className?: string;
   isRegister?: boolean;
 };
 
@@ -21,29 +19,32 @@ export default function PetBlock({
   tab,
   desk,
   alt,
-  // className,
   isRegister = true,
 }: PetBlockProps) {
+  const altStyles = alt.trim().toLowerCase().includes("dog");
+
   return (
-    <div className={css.background}>
+    <div className={`${css.background} ${altStyles ? css.altStyle : ""}`}>
       <Icon className={css.backgroundElement} iconName="img-background" />
-      <div className={css.infoCard}>
-        <span className={css.cardAvatar}>{isRegister ? "🐈" : "🐶"}</span>
-        <div className={css.text}>
-          <div className={css.topText}>
-            <h2 className={css.heading}>{isRegister ? "Jack" : "Rich"}</h2>
-            <p className={css.bDay}>
-              <span className={css.accent}>Birthday:</span>
-              {isRegister ? "18.10.2021" : "21.09.2020"}
+      {!altStyles && (
+        <div className={css.infoCard}>
+          <span className={css.cardAvatar}>{isRegister ? "🐈" : "🐶"}</span>
+          <div className={css.text}>
+            <div className={css.topText}>
+              <h2 className={css.heading}>{isRegister ? "Jack" : "Rich"}</h2>
+              <p className={css.bDay}>
+                <span className={css.accent}>Birthday:</span>
+                {isRegister ? "18.10.2021" : "21.09.2020"}
+              </p>
+            </div>
+            <p className={css.paragraph}>
+              {isRegister
+                ? "Jack is a gray Persian cat with green eyes. He loves to be pampered and groomed, and enjoys playing with toys."
+                : "Rich would be the perfect addition to an active family that loves to play and go on walks. I bet he would love having a doggy playmate too!"}
             </p>
           </div>
-          <p className={css.paragraph}>
-            {isRegister
-              ? "Jack is a gray Persian cat with green eyes. He loves to be pampered and groomed, and enjoys playing with toys."
-              : "Rich would be the perfect addition to an active family that loves to play and go on walks. I bet he would love having a doggy playmate too!"}
-          </p>
         </div>
-      </div>
+      )}
       <picture className={css.picture}>
         <source
           media="(min-width:1280px)"
@@ -53,11 +54,11 @@ export default function PetBlock({
           media="(min-width:768px)"
           srcSet={`${tab.src1x} 1x, ${tab.src2x} 2x`}
         />
-        <Image
+        <img
           width={334}
           height={280}
           className={css.image}
-          src={mob.src1x}
+          srcSet={`${mob.src1x} 1x, ${mob.src2x} 2x`}
           alt={alt}
           loading="eager"
         />
