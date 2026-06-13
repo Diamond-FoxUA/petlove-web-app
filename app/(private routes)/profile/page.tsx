5c05581288cc08bce19e860a4d57f2aafd87c585
Dmytro Farbun <dimofarb@gmail.com>
2026-06-13 11:46:42 +0300

    feat: add profileContainer styles and update Profile component to use new CSS class

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -3,7 +3,7 @@ import UserCard from "@/app/features/profile/components/UserCard/UserCard";
 
 export default function Profile () {
   return (
-    <section className={`container ${css.container}`}>
+    <section className={`container ${css.profileContainer}`}>
       <UserCard />
       {/* MyNotices */}
     </section>
