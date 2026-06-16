0aa7e28a98b704dfdb35a99d159164c6a1a9f6cf
Dmytro Farbun <dimofarb@gmail.com>
2026-06-16 11:41:32 +0300

    fix: enhance accessibility and semantics in NewsItem component

--- app/features/news/components/NewsItem/NewsItem.tsx
+++ app/features/news/components/NewsItem/NewsItem.tsx
@@ -13,17 +13,24 @@ export default function NewsItem({
   imgUrl,
 }: Omit<NewsItem, "_id" | "id">) {
   const formattedDate = new Date(date).toLocaleDateString("en-GB");
+  const machineDate = new Date(date).toISOString().split("T")[0];
 
   return (
     <article className={css.article}>
-      <Link href={url} target="_blank">
+      <Link
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        tabIndex={-1}
+        aria-hidden
+      >
         <Image
           className={css.image}
           src={imgUrl}
           width={335}
           height={190}
           loading="lazy"
-          alt={title}
+          alt=""
         />
       </Link>
 
@@ -35,10 +42,19 @@ export default function NewsItem({
       <p className={css.paragraph}>{text}</p>
 
       <div className={css.articleFooter}>
-        <span className={css.dateText}>{formattedDate}</span>
-        <Link className={css.readMoreLink} href={url} target="_blank">
+        <time dateTime={machineDate} className={css.dateText}>
+          {formattedDate}
+        </time>
+        <Link
+          className={css.readMoreLink}
+          href={url}
+          rel="noopener noreferrer"
+          aria-label={`Read more about: ${title}`}
+          target="_blank"
+        >
           Read more
         </Link>
+        <span className="sr-only"> (opens in a new tab)</span>
       </div>
     </article>
   );
