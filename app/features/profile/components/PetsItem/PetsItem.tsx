5107441f20f2956c530fb36a6aa677424214db02
Dmytro Farbun <dimofarb@gmail.com>
2026-06-18 22:54:51 +0300

    feat: enhance PetsItem and PetsList components with improved styling, image handling, and responsive design

--- app/features/profile/components/PetsItem/PetsItem.tsx
+++ app/features/profile/components/PetsItem/PetsItem.tsx
@@ -25,34 +25,46 @@ export default function PetsItem({ pet, deleteUserPet }: PetsItemProps) {
         />
       </button>
 
-      <Image
-        src={pet.imgURL}
-        alt=""
-        className={css.petImage}
-        loading="eager"
-        width={66}
-        height={66}
-      />
+      <div className={css.imageWrapper}>
+        <Image
+          src={pet.imgURL}
+          alt=""
+          className={css.petImage}
+          loading="eager"
+          fill
+          sizes="66px"
+        />
+      </div>
 
       <div className={css.petInfoContainer}>
-        <h3 className={css.petTitle}>{pet.title}</h3>
+        <h3
+          className={css.petTitle}
+        >{`${pet.title.split("")[0].toUpperCase()}${pet.title.slice(1)}`}</h3>
 
         <dl className={css.petDescriptionList}>
           <div className={css.petDescriptionItem}>
             <dt className={css.descKey}>Name</dt>
-            <dd className={css.descData}>{pet.name}</dd>
+            <dd
+              className={css.descData}
+            >{`${pet.name.split("")[0].toUpperCase()}${pet.name.slice(1)}`}</dd>
           </div>
           <div className={css.petDescriptionItem}>
             <dt className={css.descKey}>Birthday</dt>
-            <dd className={css.descData}>{pet.birthday}</dd>
+            <dd className={css.descData}>
+              {new Date(pet.birthday).toLocaleDateString().split("/").join(".")}
+            </dd>
           </div>
           <div className={css.petDescriptionItem}>
             <dt className={css.descKey}>Sex</dt>
-            <dd className={css.descData}>{pet.sex}</dd>
+            <dd
+              className={css.descData}
+            >{`${pet.sex.split("")[0].toUpperCase()}${pet.sex.slice(1)}`}</dd>
           </div>
           <div className={css.petDescriptionItem}>
             <dt className={css.descKey}>Species</dt>
-            <dd className={css.descData}>{pet.species}</dd>
+            <dd
+              className={css.descData}
+            >{`${pet.species.split("")[0].toUpperCase()}${pet.species.slice(1)}`}</dd>
           </div>
         </dl>
       </div>
