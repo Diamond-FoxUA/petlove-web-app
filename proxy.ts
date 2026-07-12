b66acd129415ca4a1f6eea72e445615dff470f7b
Dmytro Farbun <dimofarb@gmail.com>
2026-07-12 20:16:53 +0300

    fix: enhance proxy logic for authentication route handling

--- proxy.ts
+++ proxy.ts
@@ -4,6 +4,9 @@ export async function proxy(request: NextRequest) {
   const token = request.cookies.get("accessToken")?.value;
   const { pathname } = request.nextUrl;
 
+  const authRoutes = ["/login", "/register"];
+  const isAuthRoute = authRoutes.some((route) => pathname.startsWith(route));
+
   const privateRoutes = ["/profile", "/add-pet"];
   const isPrivateRoute = privateRoutes.some((route) =>
     pathname.startsWith(route),
@@ -13,9 +16,13 @@ export async function proxy(request: NextRequest) {
     return NextResponse.redirect(new URL("/register", request.url));
   }
 
+  if (isAuthRoute && token) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/profile/:path*", "/add-pet/:path*"],
+  matcher: ["/profile/:path*", "/add-pet/:path*", "/login", "/register"],
 };
